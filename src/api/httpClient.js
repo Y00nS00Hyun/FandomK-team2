b12a93da502412d66335482aec88d09cae8e84d9
JuhyeokC <bos2ablues@gmail.com>
2024-06-10 11:44:30 +0900

    ✨ api functions

---
+++ src/api/httpClient.js
@@ -0,0 +1,153 @@
+/**
+ * @function convertURL
+ * @param {String} url - The server URL.
+ * @param {Object} params - Query parameters as key-value pairs.
+ * @returns {String} - URL String with Query parameters.
+ * @throws Will throw an error with the URL is empty.
+ */
+const convertURL = (url, params) => {
+	if (!url) throw new Error('URL is Empty');
+	const query = params ? '?' + new URLSearchParams(params).toString() : '';
+	return url + query;
+};
+
+/**
+ * HTTP requests
+ *
+ * @namespace httpClient
+ */
+const httpClient = {
+	/**
+	 * HTTP GET request
+	 *
+	 * @async
+	 * @function get
+	 * @memberof httpClient
+	 * @param {String} url - The server URL.
+	 * @param {Object} params - Query parameters as key-value pairs.
+	 * @returns {Promise<Object>} The response data parsed as JSON.
+	 * @throws Will throw an error with the specified error message and error details if the request fails.
+	 */
+	get: async function (url, params) {
+		try {
+			const convertedURL = convertURL(url, params);
+			const response = await fetch(convertedURL, {
+				method: 'GET',
+			});
+			if (!response.ok) throw new Error(response.status);
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			throw Error(error);
+		}
+	},
+
+	/**
+	 * HTTP POST request
+	 *
+	 * @async
+	 * @function post
+	 * @memberof httpClient
+	 * @param {String} url - The server URL.
+	 * @param {Object} body - The request body to be sent.
+	 * @returns {Promise<Object>} The response data parsed as JSON.
+	 * @throws Will throw an error with the specified error message and error details if the request fails.
+	 */
+	post: async function (url, body) {
+		try {
+			const isFormData = body instanceof FormData;
+			const headers = isFormData ? {} : { 'Content-Type': 'application/json' };
+			const response = await fetch(url, {
+				method: 'POST',
+				headers: headers,
+				body: isFormData ? body : JSON.stringify(body),
+			});
+			if (!response.ok) throw new Error(response.status);
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			throw Error(error);
+		}
+	},
+
+	/**
+	 * HTTP PUT request
+	 *
+	 * @async
+	 * @function put
+	 * @memberof httpClient
+	 * @param {String} url - The server URL.
+	 * @param {Object} body - The request body to be sent.
+	 * @returns {Promise<Object>} The response data parsed as JSON.
+	 * @throws Will throw an error with the specified error message and error details if the request fails.
+	 */
+	put: async function (url, body) {
+		try {
+			const response = await fetch(url, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(body),
+			});
+			if (!response.ok) throw new Error(response.status);
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			throw Error(error);
+		}
+	},
+
+	/**
+	 * HTTP PATCH request
+	 *
+	 * @async
+	 * @function patch
+	 * @memberof httpClient
+	 * @param {String} url - The server URL.
+	 * @param {Object} body - The request body to be sent.
+	 * @returns {Promise<Object>} The response data parsed as JSON.
+	 * @throws Will throw an error with the specified error message and error details if the request fails.
+	 */
+	patch: async function (url, body) {
+		try {
+			const response = await fetch(url, {
+				method: 'PATCH',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(body),
+			});
+			if (!response.ok) throw new Error(response.status);
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			throw Error(error);
+		}
+	},
+
+	/**
+	 * HTTP DELETE request
+	 *
+	 * @async
+	 * @function delete
+	 * @memberof httpClient
+	 * @param {String} url - The server URL.
+	 * @returns {Promise<Object>} The response data parsed as JSON.
+	 * @throws Will throw an error with the specified error message and error details if the request fails.
+	 */
+	delete: async function (url) {
+		try {
+			const response = await fetch(url, {
+				method: 'DELETE',
+			});
+			if (!response.ok) throw new Error(response.status);
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			throw Error(error);
+		}
+	},
+};
+
+export default httpClient;
---
+++ src/api/idolsApi.js
@@ -0,0 +1,81 @@
+import httpClient from './httpClient';
+
+const API_URL = 'https://fandom-k-api.vercel.app/2-2';
+const ERROR_MESSAGES = {
+	response: '아이돌 데이터를 가져오는데 실패했습니다.',
+	id: '아이돌 id를 입력해주세요.',
+	profile: '프로필이미지는 필수값입니다.',
+};
+
+/**
+ * 아이돌 목록을 가져옵니다.
+ *
+ * @function getIdolList
+ * @param {number} pageSize - (기본값: 10) 페이지 사이즈, 데이터 갯수
+ * @param {number} cursor - (옵션) 커서, 이전에 호출한 데이터의 nextCursor
+ * @param {string} keyword - (옵션) 검색 키워드, [ 이름, 그룹 ]
+ * @returns {Promise<Object>} The response data parsed as JSON.
+ * @example
+ * const deviceType = useMediaQuery();
+ * const pageSize = PAGE_SIZES(deviceType);
+ *
+ * // 사이즈로만 호출
+ * const result = await getIdolList({ pageSize });
+ *
+ * // 커서를 이용하여 추가로 데이터 호출할 때
+ * const result = await getIdolList({ pageSize, cursor: '이전에 호출한 데이터의 nextCursor' });
+ *
+ * // 아이돌 이름이나 그룹으로 검색할 때
+ * const result = await getIdolList({ pageSize, keyword: '검색어 state' });
+ */
+export const getIdolList = async ({ pageSize = 10, ...args }) => {
+	return await httpClient.get(`${API_URL}/idols`, { pageSize, ...args }).catch((e) => {
+		throw new Error(ERROR_MESSAGES.response, e);
+	});
+};
+
+/**
+ * 아이돌 정보를 업데이트합니다.
+ *
+ * @function updateIdolData
+ * @param {number} id - 아이돌 ID
+ * @param {object} body - 아이돌 업데이트 요청 바디, *profilePicture 값 필수.
+ * @param {string} body.profilePicture - (필수) 아이돌의 프로필이미지
+ * @param {string} body.name - (옵션) 아이돌의 이름
+ * @param {string} body.group - (옵션) 아이돌의 그룹
+ * @param {string} body.gender - (옵션) 아이돌의 성별
+ * @returns {Promise<Object>} The response data parsed as JSON.
+ * @example
+ * // 아이돌 이름 수정
+ * const result = await updateIdolData(id, { profilePicture, name: '이름' });
+ *
+ * // 아이돌 그룹 수정
+ * const result = await updateIdolData(id, { profilePicture, group: '이름' });
+ *
+ * // 아이돌 성별 수정
+ * const result = await updateIdolData(id, { profilePicture, gender: '성별' });
+ */
+const updateIdolData = async (id, body) => {
+	if (!id) throw new Error(ERROR_MESSAGES.id);
+	if (body.profilePicture) throw new Error(ERROR_MESSAGES.profile);
+	return await httpClient.put(`${API_URL}/idols/${id}`, body).catch((e) => {
+		throw new Error(ERROR_MESSAGES, e);
+	});
+};
+
+/**
+ * 아이돌을 삭제합니다.
+ *
+ * @function deleteIdolData
+ * @param {number} id - 삭제할 아이돌 ID
+ * @returns {Promise<Object>} The response data parsed as JSON.
+ * @example
+ * // 아이돌 삭제
+ * const result = await updateIdolData(id);
+ */
+const deleteIdolData = async (id) => {
+	if (!id) throw new Error(ERROR_MESSAGES.id);
+	return await httpClient.delete(`${API_URL}/idols/${id}`).catch((e) => {
+		throw new Error(ERROR_MESSAGES, e);
+	});
+};
