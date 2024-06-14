bf65c6d8da29d22e8c07e107d2c55e27f84381ff
오조환 <whghks1020@naver.com>
2024-06-15 02:04:03 +0900

    fandom-k create modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { Outlet, useLocation } from 'react-router-dom';
-import RootHeader from './layout/RootHeader';
+import { Outlet, useLocation } from "react-router-dom";
+import RootHeader from "./layout/RootHeader";
 
 function App() {
-	const { pathname } = useLocation();
-	return (
-		<>
-			{pathname !== '/' && <RootHeader />}
-			<main id='rootContainer'>
-				<Outlet />
-			</main>
-		</>
-	);
+  const { pathname } = useLocation();
+  return (
+    <>
+      {pathname !== "/" && <RootHeader />}
+      <main id="rootContainer">
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
 export default App;
