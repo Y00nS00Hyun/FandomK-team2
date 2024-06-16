9d73356602b4c7bc267839e7189571f20914a970
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-17 07:22:53 +0900

    버튼 블록으로 감싸서 위치 조정

--- src/pages/ListPage/DonationWaitList/DonationList/CardDecoration.js
+++ src/pages/ListPage/DonationWaitList/DonationList/CardDecoration.js
@@ -75,19 +75,18 @@ const Img = styled.img`
 	object-position: center;
 `;
 
-const BlackGradation = styled.img`
-	width: ${({ size }) => CARD_WIDTHS[size] ?? CARD_WIDTHS["small"]};
-	border-radius: 8px;
-	position: absolute;
-	bottom: 0;
-`;
-
 const Block = styled.div`
 	display: block;
 	bottom: 8px;
 	position: absolute;
 	left: 50%;
 	transform: translateX(-50%);
+`;
+
+const SubmitButton = styled(Button)`
+	font-size: 13px;
+	width: ${({ size }) => BUTTON_WIDTHS[size] ?? BUTTON_WIDTHS["small"]};
+	height: ${({ size }) => BUTTON_HEIGHTS[size] ?? BUTTON_HEIGHTS["small"]};
 `;
 
 const SubmitButton = styled(Button)`
@@ -155,6 +154,6 @@ const StatusInfo = styled.div`
 
 // 😸 export 해보자 😸
 
-const style = { Card, Img, BlackGradation, SubmitButton, ImgButton, Subtitle, Title, Detail, InfoWrapper, Status, Credit, Countdown, StatusInfo, Block };
+const style = { Card, Img, SubmitButton, ImgButton, Subtitle, Title, Detail, InfoWrapper, Status, Credit, Countdown, StatusInfo, Block };
 
 export default style;
