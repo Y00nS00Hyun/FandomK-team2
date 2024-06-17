d412be978cc478444ebc1490b07c7585a6ebe0bc
JuhyeokC <bos2ablues@gmail.com>
2024-06-17 17:28:34 +0900

    ✨ 크레딧 state 부모로 올림

--- src/pages/ListPage/ListPage.jsx
+++ src/pages/ListPage/ListPage.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MyCredit from "./MyCredit/MyCredit";
 import DonationWaitList from "./DonationWaitList/DonationWaitList";
 import ChartOfMonth from "./ChartOfMonth/ChartOfMonth";
 import useMediaQuery from "../../hooks/useMediaQuery";
 
+const CREDIT_NAME = "myCredit";
+
 function ListPage() {
 	const mode = useMediaQuery();
+	const [myCredit, setMyCredit] = useState(() => (localStorage?.getItem(CREDIT_NAME) === null ? 0 : Number(localStorage.getItem(CREDIT_NAME))));
+
+	useEffect(() => {
+		localStorage.setItem(CREDIT_NAME, myCredit);
+	}, [myCredit]);
 
 	return (
 		<article>
-			<MyCredit />
-			<DonationWaitList mode={mode} />
+			<MyCredit mode={mode} creditState={[myCredit, setMyCredit]} />
+			<DonationWaitList mode={mode} myCredit={myCredit} />
 			<ChartOfMonth mode={mode} />
 		</article>
 	);
