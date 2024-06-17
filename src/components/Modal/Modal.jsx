7f7daf63b114eb1b7f529f5da95d79bceec5d9ae
JuhyeokC <bos2ablues@gmail.com>
2024-06-17 17:26:16 +0900

    🎨 Xbutton 컴포넌트 적용

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,7 @@
 // Modal.js
 import React from "react";
 import Button from "../Button/Button";
-import CloseIcon from "../../assets/images/icon/icon-X.svg";
+import Xbutton from "../Button/Xbutton";
 import "./Modal.css";
 
 function Modal({ show, icon, buttonAction, buttonName, onClose, children, title }) {
@@ -17,9 +17,8 @@ function Modal({ show, icon, buttonAction, buttonName, onClose, children, title 
 			<div className="modal-content" onClick={(e) => e.stopPropagation()}>
 				<div className="modal-head">
 					<p className="modal-title">{title}</p>
-					<button className="modal-close" onClick={onClose}>
-						<img src={CloseIcon} alt="닫기" />
-					</button>
+
+					<Xbutton className="modal-close" onClick={onClose}></Xbutton>
 				</div>
 				<div className="modal-body">{children}</div>
 				<div className="modal-foot">
