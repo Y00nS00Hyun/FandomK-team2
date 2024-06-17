b704b455cbc48d3c1dc004a7b7e87a9a1a1de6d8
JuhyeokC <bos2ablues@gmail.com>
2024-06-17 11:54:19 +0900

    🎨 후원 페이지 useAsync 수정에 맞춰 구조 수정 및 주석 추가

--- src/pages/ListPage/DonationWaitList/DonationList/DonationList.jsx
+++ src/pages/ListPage/DonationWaitList/DonationList/DonationList.jsx
@@ -5,32 +5,64 @@ import LodingImage from "../../../../components/LodingImage/LodingImage";
 import CaretButton from "../../../../components/CaretButton/CaretButton";
 import Card from "./DonationCard";
 
+/**
+ * @JuhyeokC
+ * mode 별 페이지사이즈 매직넘버
+ */
+const PAGE_SIZES = {
+	desktop: 4,
+	others: 3,
+};
+
 function DonationList({ mode }) {
+	/**
+	 * @JuhyeokC
+	 * 페이지 사이즈
+	 * mode 는 부모컴포넌트에서 사용된 useMediaQuery 입니다~
+	 * 부모컴포넌트에서 현재컴포넌트로 가져오셔도 됩니다!
+	 */
+	const pageSize = PAGE_SIZES[mode];
 
-	// list items
-	const [items, setItems] = useState([]);
+	/**
+	 * @JuhyeokC
+	 * useAsync 커스텀훅 사용
+	 */
+	const { refetchFunction, data, pending, error } = useAsync(getDonationList);
 
-	// async controller
-	const [isLoading, errorMessage, runFunction] = useAsync(getDonationList);
-
+	/**
+	 * @JuhyeokC
+	 * 렌더링 된 후 fetch 함수 실행
+	 */
 	useEffect(() => {
-		// 비동기 호출을 위한 래퍼 함수
-		const fetchDonations = async () => {
-			const result = await getDonationList({});
-			setItems(result.list);
-		};
-		fetchDonations();
-	}, []);
+		refetchFunction({ pageSize });
+	}, [refetchFunction, pageSize]);
+
+	/**
+	 * @JuhyeokC
+	 * data 가 업데이트될 때 list가 담길 items
+	 */
+	const items = data?.list || [];
+
 	return (
 		<div>
-			<div>
-				{isLoading && <LodingImage />}
+			{/**
+			 * @JuhyeokC
+			 * 로딩 출력
+			 */}
+			{pending && <LodingImage />}
 
-				{items?.map((item) => (
-					<Card item={item} key={item.id} />
-				))}
-				{errorMessage && <p>에러발생!!!!!!</p>}
-			</div>
+			{/**
+			 * @JuhyeokC
+			 * 에러 출력
+			 */}
+			{error && <p>{error.message}에러발생!!!!!!</p>}
+
+			{/**
+			 * @JuhyeokC
+			 * 데이터 출력
+			 */}
+			{items && items.map((item) => <Card item={item} key={item.id} />)}
+
 			{/* <div>
 				<CaretButton direction="LEFT" size="normal" />
 				<CaretButton direction="RIGHT" size="normal" />
@@ -40,3 +72,9 @@ function DonationList({ mode }) {
 }
 
 export default DonationList;
+
+/**
+ * @JuhyeokC
+ * 확인 후 제 이름이 달린 주석은 삭제해주세요!
+ * 이해가 어려운 부분은 질문해주세요!
+ */
