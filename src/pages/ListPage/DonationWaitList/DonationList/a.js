dd392cafc722b669563a380075c4a7d3d5a72ee0
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-18 07:46:28 +0900

    미흡

--- src/pages/ListPage/DonationWaitList/DonationList/a.js
+++ src/pages/ListPage/DonationWaitList/DonationList/a.js
@@ -17,7 +17,7 @@ const settings = {
 	rows: 1, //이미지를 몇 줄로 표시할지 개수
 	dots: false, //슬라이더 아래에 도트 네비게이션 버튼 표시 여부
 	draggable: false, //슬라이드 드래그 가능여부
-	arrows: true, //이전 다음 버튼 표시 여부 -> 왜 안생기냐~ 환장하겠네
+	arrows: true, //이전 다음 버튼 표시 여부
 	speed: 500,
 	slidesToShow: 4,
 	//centerMode: true, //중앙에 슬라이드가 보여지는 모드 -> 왜 중앙으로 안가?????
@@ -31,7 +31,7 @@ const settings = {
 				arrows: false, //이전 다음 버튼 표시 여부
 				draggable: true, //슬라이드 드래그 가능여부
 				slidesToShow: 3,
-				slidesToScroll: 1,
+				slidesToScroll: 3,
 				dots: true,
 				centerMode: true,
 			},
