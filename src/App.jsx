01acc3ea8d217e779c302784ec51ade45654862e
JuhyeokC <bos2ablues@gmail.com>
2024-06-17 14:56:00 +0900

    💄 RootHeader 스타일링, 배경 데코 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { useState, useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
-import RootHeader from "./layout/RootHeader";
-import LodingImage from "./components/LodingImage/LodingImage";
 import styled from "styled-components";
+import RootHeader from "./layout/RootHeader/RootHeader";
+import LodingImage from "./components/LodingImage/LodingImage";
 import DecotationImage from "./assets/images/decoration/decoration-background-top-design.svg";
 
-/**
- * @todo
- * BackgroundDecoration 컴포넌트로 보내기
- */
-const Image = styled.img`
-	position: fixed;
-	z-index: ${({ $index }) => $index};
-	top: 0;
-	left: 0;
+const HEADER_HEIGHT = 80;
+
+const Main = styled.main`
+	padding-top: ${HEADER_HEIGHT}px !important;
+	background-image: url(${DecotationImage});
+	background-repeat: no-repeat;
+	background-position: left top;
+	background-size: auto 480px;
 `;
 
 function App() {
@@ -32,11 +31,10 @@ function App() {
 				<LodingImage />
 			) : (
 				<>
-					{pathname !== "/" && <RootHeader />}
-					<Image src={DecotationImage} alt="배경 장식" draggable="false" height={480} $index={pathname !== "/" ? -1 : 0} />
-					<main id="rootContainer">
+					{pathname !== "/" && <RootHeader headerHeight={HEADER_HEIGHT} />}
+					<Main id="rootContainer">
 						<Outlet />
-					</main>
+					</Main>
 				</>
 			)}
 		</>
