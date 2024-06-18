5b2efa76928ed7cb06a0c380343a8f3aa23495a6
JuhyeokC <bos2ablues@gmail.com>
2024-06-18 22:41:51 +0900

    🔨 Apply Stash

---
+++ src/components/TitleSection/TitleSection.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+import { isEmpty } from "lodash";
+import CaretButton from "../CaretButton/CaretButton";
+
+const DEFAULT_MAX_WIDTH = 1200;
+
+const Article = styled.article`
+	margin: 0 auto;
+	padding: ${({ $bottomLine }) => ($bottomLine ? 40 : 16)}px 0;
+	min-height: 320px;
+`;
+
+const Head = styled.section`
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: space-between;
+	align-items: center;
+	gap: 16px;
+	margin: 0 auto;
+	max-width: ${DEFAULT_MAX_WIDTH}px;
+	padding-bottom: ${({ $action }) => ($action ? 32 : 24)}px;
+`;
+
+const Body = styled.section`
+	margin: 0 auto;
+	max-width: ${DEFAULT_MAX_WIDTH}px;
+`;
+
+const Content = styled.article`
+	position: relative;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const Title = styled.p`
+	flex: 1;
+	font-size: 24px;
+	font-weight: 700;
+`;
+
+const Buttons = styled.section``;
+
+/**
+ *
+ * @param {Object} title 영역 제목
+ * @param {Object} action 영역 제목
+ * @returns
+ */
+function TitleSection({ children, title, action, bottomLine }) {
+	return (
+		<Article ㄴ={bottomLine}>
+			<Head $action={isEmpty(action)}>
+				<Title>{title}</Title>
+				{action && <Buttons>{action}</Buttons>}
+			</Head>
+			<Body $bottomLine={bottomLine}>
+				<Content>{children}</Content>
+			</Body>
+		</Article>
+	);
+}
+
+export default TitleSection;
