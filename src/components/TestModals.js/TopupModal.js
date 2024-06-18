92d2602b5660c3fca3a67aa5abb83b1eb307adac
오조환 <whghks1020@naver.com>
2024-06-18 18:08:00 +0900

    1. modifying the votes, topup modal design 2. add Xbutton component prop 3. remove the donation modal input button

--- src/components/TestModals.js/TopupModal.js
+++ src/components/TestModals.js/TopupModal.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { Children } from "react";
 import InputRadio from "../InputRadio/InputRadio";
 import credit from "../../assets/images/icon/icon-credit.svg";
 import topup from "../TestModalModule.css/Topup.module.css";
+import Xbutton from "../Button/Xbutton";
+import Button from "../Button/Button";
 
 const CREDIT_UNITS = [{ unit: 100 }, { unit: 500 }, { unit: 1000 }];
 
 function TopupModal({ handleChange }) {
 function TopupModal({ handleChange }) {
 	return (
-		<div className={topup.creditChargeContainer}>
-			<div className={topup.chargeBody}>
+		<div className={topup.container}>
+			<Xbutton size={"small"} className={topup.top}>
+				크레딧 충전하기
+			</Xbutton>
+			<div className={topup.body}>
 				{CREDIT_UNITS.map(({ unit }) => (
 					<InputRadio key={`credit-unit-${unit}`} className={topup.creditChooseButton} id={`creditUnit${unit}`} name={"creditUnit"} value={unit} onChange={handleChange}>
 						<div className={topup.creditAmount}>
@@ -19,6 +24,9 @@ function TopupModal({ handleChange }) {
 					</InputRadio>
 				))}
 			</div>
+			<Button icon={"credit"} size={"wide"} className={topup.button}>
+				충전하기
+			</Button>
 		</div>
 	);
 }
