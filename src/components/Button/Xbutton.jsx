b588ffdfaef33e52a90759fbdead1d0996d6807d
오조환 <whghks1020@naver.com>
2024-06-18 18:08:00 +0900

    1. modifying the votes, topup modal design 2. add Xbutton component prop 3. remove the donation modal input button

--- src/components/Button/Xbutton.jsx
+++ src/components/Button/Xbutton.jsx
@@ -1,3 +1,4 @@
+import { Children } from "react";
 import xButton from "../../assets/images/icon/icon-X.svg";
 import styled from "styled-components";
 
@@ -13,14 +14,20 @@ const X_BUTTON_HEIGHTS = {
 	large: "15.5px",
 };
 
-// 사용 방법 : 닫기 버튼을 사용할 컴포넌트에서 <Xbutton $size={"small"} /> 형식으로 size 프롭으로 원하는 크기의 값을 전달
+// 사용 방법 : 닫기 버튼을 사용할 컴포넌트에서 프롭으로 설정 값 전달
+// ex. <Xbutton size={"large"} className={section에 설정할 클래스}>칠드런 프롭 내용 입력</Xbutton>
 const IMG = styled.img`
 	width: ${({ size }) => X_BUTTON_WIDTHS[size] ?? X_BUTTON_WIDTHS["small"]};
 	height: ${({ size }) => X_BUTTON_HEIGHTS[size] ?? X_BUTTON_HEIGHTS["small"]};
 	cursor: pointer;
 `;
 
-function Xbutton({ size, onClick }) {
-	return <IMG src={xButton} width={X_BUTTON_WIDTHS[size]} height={X_BUTTON_HEIGHTS[size]} onClick={onClick} alt="닫기 버튼" />;
+function Xbutton({ size, onClick, className, children }) {
+	return (
+		<section className={className}>
+			{children}
+			<IMG src={xButton} width={X_BUTTON_WIDTHS[size]} height={X_BUTTON_HEIGHTS[size]} onClick={onClick} alt="닫기 버튼" />
+		</section>
+	);
 }
 export default Xbutton;
