7238f48e46e482c5ef9c16cbd88c45e8c3f446f0
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-19 16:06:18 +0900

    수정수정수정

--- src/pages/ListPage/DonationWaitList/DonationList/a.js
+++ src/pages/ListPage/DonationWaitList/DonationList/a.js
@@ -35,7 +35,7 @@ const settings = {
 	//draggable: false, //슬라이드 드래그 가능여부
 	arrows: false, //이전 다음 버튼 표시 여부
 	speed: 500,
-	//slidesToShow: 4, //centerMode: true, //중앙에 슬라이드가 보여지는 모드 -> 왜 중앙으로 안가?????
+	//slidesToShow: 4, //centerMode: true,
 	centerPadding: "0px", //중앙에 슬라이드가 보여지는 모드에서 패딩 값
 	infinite: false,
 	variableWidth: true, // 👽 12시간만에 찾아낸 width 조정 해결책 ㅠㅠㅠㅠㅠ
@@ -59,5 +59,5 @@ const settings = {
 export default settings;
 
 // 중요
-// 모바일&탭 사이즈일 때 왜 한 번 스크롤 해야 생기는걸까?
-// 저장할 때 마다 바가 하나씩 생기는 이유
+// 모바일&탭 사이즈일 때 왜 한 번 스크롤 해야 생기는걸까? (해결)
+// 저장할 때 마다 바가 하나씩 생기는 이유 (해결)
--- src/pages/ListPage/DonationWaitList/DonationWaitList.jsx
+++ src/pages/ListPage/DonationWaitList/DonationWaitList.jsx
@@ -5,7 +5,6 @@ import Slider from "react-slick";
 import TitleSection from "../../../components/TitleSection/TitleSection";
 import LodingImage from "../../../components/LodingImage/LodingImage";
 import Card from "./DonationList/DonationCard.jsx";
-import settings from "./DonationList/a.js";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -62,6 +61,32 @@ function DonationWaitList({ mode, myCreditState }) {
 	useEffect(() => {
 		getDataList();
 	}, [getDataList, load]);
+
+	const settings = {
+		rows: 1, //이미지를 몇 줄로 표시할지 개수
+		dots: false, //슬라이더 아래에 도트 네비게이션 버튼 표시 여부
+		//draggable: false, //슬라이드 드래그 가능여부
+		arrows: false, //이전 다음 버튼 표시 여부
+		speed: 500,
+		centerPadding: "0px", //중앙에 슬라이드가 보여지는 모드에서 패딩 값
+		infinite: false,
+		variableWidth: true, // 👽 12시간만에 찾아낸 width 조정 해결책 ㅠㅠㅠㅠㅠ
+		responsive: [
+			{
+				//작은 사이즈
+				breakpoint: 1200,
+				settings: {
+					arrows: false, //이전 다음 버튼 표시 여부
+					draggable: true, //슬라이드 드래그 가능여부
+					//slidesToShow: 3,
+					slidesToScroll: "auto",
+					dots: true,
+					centerMode: true,
+					infinite: false,
+				},
+			},
+		],
+	};
 
 	return (
 		<TitleSection title={"후원을 기다리는 조공"} carousel={true} size={"normal"}>
