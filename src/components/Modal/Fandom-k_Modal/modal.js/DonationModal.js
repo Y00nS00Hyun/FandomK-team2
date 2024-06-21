6b4b60361890183874da478022eb29f9e66b319b
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-22 03:12:15 +0900

    모달창 충돌 해결

--- src/components/Modal/Fandom-k_Modal/modal.js/DonationModal.js
+++ src/components/Modal/Fandom-k_Modal/modal.js/DonationModal.js
@@ -6,27 +6,11 @@ import Xbutton from "../../../Button/Xbutton";
 // 후원하기 모달
 function DonationModal({ onClose, selectedItem }) {
 	return (
-		<div id={done.modalBackground}>
-			<div className={done.donationContainer}>
-				<Xbutton size="small" className={done.donationTop} onClick={onClose}>
-					후원하기
-				</Xbutton>
-				<div className={done.donationBody}>
-					<img src={`${selectedItem.idol.profilePicture}`} className={done.donationImg} alt="연예인 프로필 사진" />
-					<div className={done.adTitle}>
-						<span className={done.adWhere}>{selectedItem.subtitle}</span>
-						<span>{selectedItem.title}</span>
-					</div>
-					<form>
-						<div className={done.creditInputBox}>
-							<input className={done.creditInput} type="number" name="chargeCredit" placeholder="크레딧 입력" min={1} max={9999} />
-							<img src={credit} alt="크레딧 사진" />
-						</div>
-						<Button type="submit" size={"wide"}>
-							후원하기
-						</Button>
-					</form>
-				</div>
+		<div className={done.donationBody}>
+			<img src={selectedItem.idol.profilePicture} className={done.donationImg} alt="연예인 프로필 사진" />
+			<div className={done.adTitle}>
+				<span className={done.adWhere}>{selectedItem.subtitle}</span>
+				<span>{selectedItem.title}</span>
 			</div>
 			<form>
 				<div className={done.creditInputBox}>
